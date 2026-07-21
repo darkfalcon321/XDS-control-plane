a853b974de4a98520ccf28f46f3c8ef3020ff984
Albin <Albin.babu@hotmail.com>
2026-07-21 08:16:11 +0800

    BREAKING CHANGE: added RDS feat: made it dynamic instead of static port and domains fix: Listener was already resolved in previous commit

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 run-envoy:
 	docker run --network=host -it -v .:/host docker.io/envoyproxy/envoy:v1.38.0 bash
+
+run-controlplane:
+	uv run python main.py
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
 import uvicorn
 
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request, Response, Header
 
 app = FastAPI()
 
@@ -36,59 +37,89 @@ CLUSTERS = [
     cluster("example", "127.0.0.1", 8050)
 ]
 
-LISTENER = {
-    "@type": "type.googleapis.com/envoy.config.listener.v3.Listener",
-    "name": "example",
-    "address": {"socket_address":{"address": "0.0.0.0","port_value":8082}},
-    "filter_chains": [
-        {
-            "filters": [        # [FILTER] robust to downstream (clients) data but not upstream (you)
+def clusters(services):
+    ret = []
+    for service in services:
+        if service["type"] != "service":
+            continue
+        backend = service["backend"]
+        ret.append(
+            cluster(service["name"] + "-cluster", backend["addr"], backend["port"])
+        )
+    return ret
+
+
+
+def route_config(services):
+    virtual_hosts = []
+    for service in services:
+        if service["type"] != "service":
+            continue
+        routes = []
+        for index, route in enumerate(service["routes"]):
+            routes.append(
                 {
-                    "name": "http traffic",
-                    "typed_config": {       
-                        "@type": "type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager",
-                        "stat_prefix": "backends",
-                        "http_filters": [
-                            {
-                                "name": "routing", 
-                                "typed_config": {
-                                    "@type": "type.googleapis.com/envoy.extensions.filters.http.router.v3.Router"
+                    "name": f"route-{index}",
+                    "match": route["match"],
+                    "route": {"cluster": service["name"] + "-cluster"}
+                }
+            )
+        virtual_hosts.append(
+            {
+                "name": service["name"],
+                "domains": service["domains"],
+                "routes": routes,
+            }
+        )
+    return {
+        "@type": "type.googleapis.com/envoy.config.route.v3.RouteConfiguration",
+        "name": "backends",
+        "virtual_hosts": virtual_hosts
+    }
+
+def listener(name: str, port: int, route_config_name: str, controlplane: str):
+    return {
+        "@type": "type.googleapis.com/envoy.config.listener.v3.Listener",
+        "name": name,
+        "address": {"socket_address": {"address": "0.0.0.0","port_value":port}},
+        "filter_chains": [
+            {
+                "filters": [        # [FILTER] robust to downstream (clients) data but not upstream (you)
+                    {
+                        "name": "http traffic",
+                        "typed_config": {       
+                            "@type": "type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager",
+                            "stat_prefix": "backends",
+                            "http_filters": [
+                                {
+                                    "name": "routing", 
+                                    "typed_config": {
+                                        "@type": "type.googleapis.com/envoy.extensions.filters.http.router.v3.Router"
+                                    }
+                                }
+                            ],
+                            "rds": {
+                                "route_config_name": route_config_name,
+                                "config_source": {
+                                    "api_config_source": {
+                                        "api_type": "REST",
+                                        "cluster_names": [controlplane],
+                                        "refresh_delay": "5s"
+                                    },
                                 }
                             }
-                        ],
-                        "route_config": {
-                            "name": "routes",
-                            "virtual_hosts": [
-                                {
-                                    "name": "example vh",
-                                    "domains": ["*"],
-                                    "routes": [
-                                        {
-                                            "name": "static",
-                                            "match": {"prefix": "/static"},
-                                            "direct_response": {
-                                                "body": {
-                                                    "inline_string": "static response"
-                                                },
-                                                "status": 200,
-                                            },
-                                        },
-                                        {
-                                            "name": "catchall",
-                                            "match": {"prefix": "/"},
-                                            "route": {"cluster": "example"}
-                                        }
-                                    ]
-                                }
-                            ]
-                        }
-                    },
-                }
-            ]
-        }
-    ]
-}
+                        },
+                    }
+                ]
+            }
+        ]
+    }
 
+
+def fetch_external_data():
+    with open("data.json") as f:
+        data = json.load(f)
+        return data
 
 
 @app.get("/greeting")
@@ -97,12 +128,24 @@ def greet(name: str = "World"):
 
 
 @app.post("/v3/discovery:{resource_type}")
-async def clusters(request: Request, resource_type: str):
-    # print(await request.json())
+async def resouces(request: Request, resource_type: str, host = Header()):
+    fake_api_data = fetch_external_data()
+
+    cluster_resources = clusters(fake_api_data)
+    route_config_resource = route_config(fake_api_data)
+    listener_resources = [
+        listener(resource["name"], resource["port"], resource["route_config"], host)
+        for resource in fake_api_data
+        if resource["type"] == "listener"
+    ]
+
+
     if resource_type == "clusters":
-        return {"version_info": "0", "resources": CLUSTERS}
+        return {"version_info": "0", "resources": cluster_resources}
     elif resource_type == "listeners":
-        return {"version_info": "0", "resources": LISTENER}
+        return {"version_info": "0", "resources": listener_resources}
+    elif resource_type == "routes":
+        return {"version_info": "0", "resources": route_config_resource}
     else:
         return Response(
             "Unsupported resource type", media_type="text/plain", status_code=400
